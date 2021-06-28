e5fc6697dc82ad0551fec7483342881b3e9e4984
felippe menezes <felippemenezes1982@gmail.com>
2021-06-28 15:55:34 -0300

    Index and Show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'characters#index'
+  get "characters", to: "characters#index"
+  get "characters/:id", to: "characters#show", as: :character
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
