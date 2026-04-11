7e1f05efc12ab3f83b330110f889c7fe82f53ec5
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-11 20:22:11 -0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,93 +1,96 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'json'
 require 'open-uri'
 
 def open_api(url)
-    puts "opening #{url}..."
-    data_serialized = URI.open(url).read
-    data = JSON.parse(data_serialized)
-    return data
+  puts "opening #{url}..."
+  data_serialized = URI.open(url).read
+  data = JSON.parse(data_serialized)
+  data
 end
 
 def open_api_model(model)
-    url = "https://swapi.dev/api/#{model}/"
-    data = open_api(url)
-    results = data["results"]
+  url = "https://swapi.dev/api/#{model}/"
+  data = open_api(url)
+  results = data["results"]
+  puts "found #{results.count} models..."
+  while data["next"]
+    data = open_api(data["next"])
+    data["results"].each { |result| results << result }
     puts "found #{results.count} models..."
-    while data["next"] do
-        data = open_api(data["next"])
-        data["results"].each do |result|
-            results << result
-        end
-        puts "found #{results.count} models..."
-    end
-    return results
+  end
+  results
 end
 
 def get_id(url)
-    url.scan(/\d/)[0].to_i
+  url.scan(/\d+/)[0].to_i
 end
 
+# FILMS
 puts "Populating Films..."
-open_api_model('films').each do | film |
-    Film.create(title: film["title"])
+open_api_model('films').each do |film|
+  Film.find_or_create_by!(title: film["title"])
 end
 
+# PLANETS
 puts "Populating Planets..."
 planets_api_results = open_api_model('planets')
 total_planets = planets_api_results.count
 hues = (0...total_planets).map { |i| (i * 360 / [total_planets, 1].max).to_i }.shuffle
 
-planets_api_results.each_with_index do | planet, index |
-    p = Planet.find_or_initialize_by(name: planet["name"])
-    p.color_hue = hues[index]
-    p.save!
+planets_api_results.each_with_index do |planet, index|
+  p = Planet.find_or_initialize_by(name: planet["name"])
+  p.color_hue = hues[index]
+  p.save!
 end
 
+# CATEGORIES (Species)
 puts "Populating Categories (Species)..."
-open_api_model('species').each do | category |
-    Category.create(name: category["name"])
+open_api_model('species').each do |category|
+  Category.find_or_create_by!(name: category["name"])
 end
 
+
+# STARSHIPS
 puts "Populating Starships..."
-open_api_model('starships').each do | starship |
-    Starship.create(name: starship["name"], model: starship["model"])
+open_api_model('starships').each do |starship|
+  Starship.find_or_create_by!(name: starship["name"]) do |s|
+    s.model = starship["model"]
+  end
 end
 
+# VEHICLES
 puts "Populating Vehicles..."
-open_api_model('vehicles').each do | vehicle |
-    Vehicle.create(name: vehicle["name"], model: vehicle["model"])
+open_api_model('vehicles').each do |vehicle|
+  Vehicle.find_or_create_by!(name: vehicle["name"]) do |v|
+    v.model = vehicle["model"]
+  end
 end
 
+# CHARACTERS (People)
 puts "Populating Characters (People)..."
-open_api_model('people').each do | person |
-    # Character.create(name: person["name"], height: person["height"], mass: person["mass"], birth_year: person["birth_year"])
-    character = Character.new
-    character.name = person["name"]
-    character.height = person["height"]
-    character.mass = person["mass"]
-    character.birth_year = person["birth_year"]
-    character.planet = Planet.find(get_id(person["homeworld"]))
-    if person["species"][0]
-        character.category = Category.find(get_id(person["species"].first))
-    else
-        character.category = Category.find(1)
-    end
-    person["films"].each do | film |
-        character.films << Film.find(get_id(film))
-    end
-    person["vehicles"].each do | vehicle |
-        character.vehicles << Vehicle.find(get_id(vehicle))
-    end
-    person["starships"].each do | starship |
-        character.starships << Starship.find(get_id(starship))
-    end
-    puts "Creating Character #{character.name}" if character.save
+open_api_model('people').each do |person|
+  character = Character.find_or_initialize_by(name: person["name"])
+
+  character.height     = person["height"]
+  character.mass       = person["mass"]
+  character.birth_year = person["birth_year"]
+  character.planet     = Planet.find_by!(name: Planet.find(get_id(person["homeworld"])).name)
+
+  character.category = if person["species"][0]
+    Category.find(get_id(person["species"].first))
+  else
+    Category.find(1)
+  end
+
+  film_ids = person["films"].map { |film| get_id(film) }
+  character.films = Film.find(film_ids)
+
+  vehicle_ids = person["vehicles"].map { |v| get_id(v) }
+  character.vehicles = Vehicle.find(vehicle_ids)
+
+  starship_ids = person["starships"].map { |s| get_id(s) }
+  character.starships = Starship.find(starship_ids)
+
+  puts "Saving Character #{character.name}..." if character.save
 end
+
