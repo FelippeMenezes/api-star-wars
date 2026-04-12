b28ed5a56242db41575717d689bb9a4e4be83ecf
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-12 18:22:26 -0300

    render-build.sh and update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,56 +32,68 @@ def open_api_model(model)
 end
 
 def get_id(url)
-    url.scan(/\d/)[0].to_i
+    return nil if url.blank?
+    url.scan(/\d+/).last.to_i
 end
 
 puts "Populating Films..."
+films_map = {}
 open_api_model('films').each do | film |
-    Film.create(title: film["title"])
+    obj = Film.find_or_create_by!(title: film["title"])
+    films_map[get_id(film["url"])] = obj
 end
 
 puts "Populating Planets..."
+planets_map = {}
 open_api_model('planets').each do | planet |
-    Planet.create(name: planet["name"])
+    obj = Planet.find_or_create_by!(name: planet["name"])
+    planets_map[get_id(planet["url"])] = obj
 end
 
 puts "Populating Categories (Species)..."
+categories_map = {}
 open_api_model('species').each do | category |
-    Category.create(name: category["name"])
+    obj = Category.find_or_create_by!(name: category["name"])
+    categories_map[get_id(category["url"])] = obj
 end
 
 puts "Populating Starships..."
+starships_map = {}
 open_api_model('starships').each do | starship |
-    Starship.create(name: starship["name"], model: starship["model"])
+    obj = Starship.find_or_create_by!(name: starship["name"], model: starship["model"])
+    starships_map[get_id(starship["url"])] = obj
 end
 
 puts "Populating Vehicles..."
+vehicles_map = {}
 open_api_model('vehicles').each do | vehicle |
-    Vehicle.create(name: vehicle["name"], model: vehicle["model"])
+    obj = Vehicle.find_or_create_by!(name: vehicle["name"], model: vehicle["model"])
+    vehicles_map[get_id(vehicle["url"])] = obj
 end
 
 puts "Populating Characters (People)..."
 open_api_model('people').each do | person |
-    # Character.create(name: person["name"], height: person["height"], mass: person["mass"], birth_year: person["birth_year"])
-    character = Character.new
-    character.name = person["name"]
+    character = Character.find_or_initialize_by(name: person["name"])
     character.height = person["height"]
     character.mass = person["mass"]
     character.birth_year = person["birth_year"]
-    character.planet = Planet.find(get_id(person["homeworld"]))
-    if person["species"][0]
-        character.category = Category.find(get_id(person["species"].first))
-    else
-        character.category = Category.find(1)
-    end
+
+    character.planet = planets_map[get_id(person["homeworld"])]
+
+    species_id = get_id(person["species"]&.first)
+    character.category = categories_map[species_id] || Category.first
+
     person["films"].each do | film |
-        character.films << Film.find(get_id(film))
+        film_obj = films_map[get_id(film)]
+        character.films << film_obj if film_obj && !character.films.include?(film_obj)
     end
     person["vehicles"].each do | vehicle |
-        character.vehicles << Vehicle.find(get_id(vehicle))
+        vehicle_obj = vehicles_map[get_id(vehicle)]
+        character.vehicles << vehicle_obj if vehicle_obj && !character.vehicles.include?(vehicle_obj)
     end
     person["starships"].each do | starship |
-        character.starships << Starship.find(get_id(starship))
+        starship_obj = starships_map[get_id(starship)]
+        character.starships << starship_obj if starship_obj && !character.starships.include?(starship_obj)
     end
     puts "Creating Character #{character.name}" if character.save
 end
