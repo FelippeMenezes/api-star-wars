3c4531a57c89607cfc2facbff9636103cc5b838f
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-12 15:53:50 -0300

    Create planets contorller index action and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'characters#index'
   get "characters", to: "characters#index"
   get "characters/:id", to: "characters#show", as: :character
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "planets", to: "planets#index"
 end
